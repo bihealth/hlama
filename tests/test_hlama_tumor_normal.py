9f2066d1677cad90fdf059a8e617cae2af874c0e
Messerschmidt <clemens.messerschmidt@charite.de>
2016-06-16 17:21:32 +0200

    add checks of HLA types for tumor/normal tests

--- tests/test_hlama_tumor_normal.py
+++ tests/test_hlama_tumor_normal.py
@@ -39,3 +39,29 @@ def test_app(report, tsv_file, data_dir):
         solution = f.read().splitlines()
 
     assert set(result) == set(solution)
+
+def test_compare_HLA():
+    with open(os.path.join(os.path.dirname(__file__), work_dir,
+        'donor1_normal.d/hla_types.txt')) as f:
+        result = f.read().splitlines()
+        assert result == ['A*02:01', 'A*02:74', 'B*15:01', 'B*53:01', 'C*04:01', 'C*07:02']
+
+    with open(os.path.join(os.path.dirname(__file__), work_dir,
+        'donor1_tumor.d/hla_types.txt')) as f:
+        result = f.read().splitlines()
+        assert result == ['A*02:01', 'A*02:74', 'B*15:01', 'B*53:01', 'C*04:01', 'C*07:02']
+
+    with open(os.path.join(os.path.dirname(__file__), work_dir,
+        'donor1_tumor_rna.d/hla_types.txt')) as f:
+        result = f.read().splitlines()
+        assert result == ['A*02:01', 'A*02:01', 'B*15:01', 'B*53:01', 'C*04:01', 'C*07:02']
+
+    with open(os.path.join(os.path.dirname(__file__), work_dir,
+        'donor2_normal.d/hla_types.txt')) as f:
+        result = f.read().splitlines()
+        assert result == ['A*01:01', 'A*24:02', 'B*27:04', 'B*27:05', 'C*02:02', 'C*18:01']
+
+    with open(os.path.join(os.path.dirname(__file__), work_dir,
+        'donor2_tumor.d/hla_types.txt')) as f:
+        result = f.read().splitlines()
+        assert result == ['A*01:01', 'A*24:02', 'B*27:04', 'B*27:05', 'C*02:02', 'C*18:01']
